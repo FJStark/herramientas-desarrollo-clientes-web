d36dbf874cd9737404f8b92a58f358c87d5a1061
FJStark <fjsalamanca@outlook.es>
2023-10-12 16:05:49 +0200

    Created get_all_users function for /users endpoint and import Controller into api router.

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -5,8 +5,17 @@ namespace App\Http\Controllers;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
+use App\Models\User;
 
 class Controller extends BaseController
 {
     use AuthorizesRequests, ValidatesRequests;
+    
+    /**
+     * Get all users stored in users table
+     */
+    public function getAllUsers(){
+        return response()->json(User::all());
+    }
 }
+
