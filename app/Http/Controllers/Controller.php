9c2bd9db2638bf5c88eb8875adcfd09bed3dc10d
FJStark <fjsalamanca@outlook.es>
2023-10-12 16:11:16 +0200

    Created function to get one user by id

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
@@ -9,4 +10,12 @@ use Illuminate\Routing\Controller as BaseController;
 class Controller extends BaseController
 {
     use AuthorizesRequests, ValidatesRequests;
+    
+    /**
+     * @param id
+     * Get one user from users table, filter by id
+     */
+    public function getUserById($id){
+        return response()->json(User::findOrFail($id));
+    }
 }
